e2eac68e716284ab59e00981a8db7439bdd3352a
Jean-Richard Latortue <rickyransomco@gmail.com>
2020-04-19 08:20:52 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,3 +11,10 @@ end
 def link
   puts "It's Dangerous To Go Alone! Take This."
 end
+
+def all_phrases
+  all_phrases = /"It's-a me, Mario!"\n/"Thank You Mario! But Our Princess Is In Another Castle!"\n/"It's Dangerous To Go Alone! Take This."
+  puts all_phrases
+end
+
+  
